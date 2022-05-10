a148649b616dca33c8d9ac285affcffb0d6aa876
staciekozhardy <stacie.hardy@noaa.gov>
2022-05-10 15:35:09 -0700

    Update Annotation_JoBSS_99_MatchBoundingBoxes.R
    Completed first round of code for matching bounding boxes

--- Annotation_JoBSS_99_MatchBoundingBoxes.R
+++ Annotation_JoBSS_99_MatchBoundingBoxes.R
@@ -1,5 +1,9 @@
 # Match bounding boxes between datasets
 
+# Variables
+method <- "minimum_distance"
+#method <- "maximum_score"
+
 # Create functions -----------------------------------------------
 # Function to install packages needed
 install_pkg <- function(x)
@@ -37,7 +41,7 @@ original <- RPostgreSQL::dbGetQuery(con, "SELECT * FROM surv_jobss.tbl_detection
   mutate(o_average_x = (o_left + o_right)/2,
          o_average_y = (o_top + o_bottom)/2)
 
-processed <- RPostgreSQL::dbGetQuery(con, "SELECT * FROM surv_jobss.tbl_detections_processed_rgb") %>% # WHERE detection_type <> \'false_positive\'
+processed <- RPostgreSQL::dbGetQuery(con, "SELECT * FROM surv_jobss.tbl_detections_processed_rgb WHERE detection_type LIKE \'%seal%\'") %>% 
   rename(p_detection = detection,
          p_left = bound_left,
          p_bottom = bound_bottom,
@@ -51,16 +55,34 @@ processed <- RPostgreSQL::dbGetQuery(con, "SELECT * FROM surv_jobss.tbl_detectio
 # Merge datasets together
 data <- original %>%
   full_join(processed, by = "image_name") %>%
-  select(image_name, o_detection, p_detection, o_left, o_right, p_left, p_right, o_top, o_bottom, p_top, p_bottom, o_score, o_detection_type, p_detection_type) %>%
+  select(image_name, o_detection, p_detection, o_left, o_right, p_left, p_right, o_top, o_bottom, p_top, p_bottom, o_score, o_detection_type, p_detection_type, o_average_x, p_average_x, o_average_y, p_average_y) %>%
   mutate(intersect_LR = ifelse(p_right < o_left | p_left > o_right, "no", "yes"),
          intersect_TB = ifelse(p_top > o_bottom | p_bottom < o_top, "no", "yes"))
 
 intersecting <- data %>%
-  filter(intersect_LR == "yes" & intersect_TB == "yes")
+  filter(intersect_LR == "yes" & intersect_TB == "yes") %>%
+  mutate(distance = (sqrt(((o_average_x - p_average_x) ^ 2) + ((o_average_y - p_average_y) ^ 2)))) %>%
+  group_by(image_name, p_detection) 
+  
+if(method == "minimum_distance") {
+  intersecting <- intersecting %>%
+    slice(which.min(distance))
+  } else if (method == "maximum_score") {
+  intersecting <- intersecting %>%
+    slice(which.max(o_score))
+  }
+
+intersecting4missing <- intersecting %>%
+  select(image_name, o_detection, p_detection)
 
 missing_o <- original %>%
-  filter(!o_detection %in% (intersecting$o_detection))
+  full_join(intersecting4missing, by = c("image_name", "o_detection")) %>%
+  filter(is.na(p_detection))
 
 missing_p <- processed %>%
-  filter(!p_detection %in% (intersecting$p_detection))
+  full_join(intersecting4missing, by = c("image_name", "p_detection")) %>%
+  filter(is.na(o_detection))
 
+# Tidy up workspace and disconnect from DB
+RPostgreSQL::dbDisconnect(con)
+rm(con, intersecting4missing, data)
